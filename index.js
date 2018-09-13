052e6084b641ea886fdd3f5a8fe6b80c02bfd01d
Sajith <sajimedia@gmail.com>
2018-09-13 11:14:31 +0530

    adding image saving codes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var base64ToImage = require('base64-to-image');
 
 app.get('/', function(req, res) {
    res.sendfile('index.html');
@@ -9,6 +10,12 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
    console.log('A user connected');
+   socket.on('image',(data)=>{
+   	var base64Str = data;
+    var path ='uploadIMG'; 
+    var optionalObj = {'fileName': 'ssss', 'type':'png'};
+    base64ToImage(base64Str,path,optionalObj); 
+   })
 });
 
 http.listen(3000, function() {
